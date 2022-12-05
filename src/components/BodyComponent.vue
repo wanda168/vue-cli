71323dc7f076c9858318172e5a8e8f833a974bf9
wanda <vdking.168@gmail.com>
2022-12-05 16:02:50 +0700

    register header, body, footer components

---
+++ src/components/BodyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img
+          src="../assets/book-and-glasses.png"
+          alt="Book and glasses"
+          class="img-fluid mt-5"
+        />
+      </div>
+    </div>
+  </div>
+</template>
