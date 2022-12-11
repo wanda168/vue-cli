ec2a43a1af401f7c3f4ea1e74216888dc73b8f8e
wanda <vdking.168@gmail.com>
2022-12-11 19:46:03 +0700

    trying out our route protection middleware

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,27 @@ export default {
       };
     }
   },
+  mounted() {
+    const payload = {
+      foo: "bar",
+    };
+
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Authorization", "Bearer " + store.token);
+
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: headers,
+    };
+
+    fetch("http://localhost:8081/admin/foo", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  },
 };
 </script>
 
