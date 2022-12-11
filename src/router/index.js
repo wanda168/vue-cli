5750cb23785e8aac8c7fd818dd9b137fe05b25c9
wanda <vdking.168@gmail.com>
2022-12-11 20:42:14 +0700

    setting up stub components and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,54 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Body from "./../components/Body.vue";
 import Login from "./../components/Login.vue";
+import Books from "./../components/Books.vue";
+import Book from "./../components/Book.vue";
+import BooksAdmin from "./../components/BooksAdmin.vue";
+import BookEdit from "./../components/BookEdit.vue";
+import Users from "./../components/Users.vue";
+import UserEdit from "./../components/UserEdit.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Body,
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: Body,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/books",
+    name: "Books",
+    component: Books,
+  },
+  {
+    path: "/books/:bookName",
+    name: "Book",
+    component: Book,
+  },
+  {
+    path: "/admin/books",
+    name: "BooksAdmins",
+    component: BooksAdmin,
+  },
+  {
+    path: "/admin/books/:bookId",
+    name: "BookEdit",
+    component: BookEdit,
+  },
+  {
+    path: "/admin/users",
+    name: "Users",
+    component: Users,
+  },
+  {
+    path: "/admin/users/:userId",
+    name: "User",
+    component: UserEdit,
+  },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
