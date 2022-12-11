b1f10bbb8b796c5f942126f0abfa822915420f59
wanda <vdking.168@gmail.com>
2022-12-11 22:22:00 +0700

    update the header coponent to link to our stub component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,6 +20,48 @@
               >Home</router-link
             >
           </li>
+
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/books">Books</router-link>
+          </li>
+
+          <li v-if="store.token !== ''" class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navBarDropDown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Admin
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navBarDropDown">
+              <li>
+                <router-link class="dropdown-item" to="/admin/users"
+                  >Manage Users</router-link
+                >
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/admin/users/0"
+                  >Add Users</router-link
+                >
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/admin/books"
+                  >Manage Books</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  class="dropdown-item"
+                  :to="{ name: 'BookEdit', params: { bookId: 0 } }"
+                  >Add Books</router-link
+                >
+              </li>
+            </ul>
+          </li>
+
           <li class="nav-item">
             <router-link v-if="store.token == ''" class="nav-link" to="/login"
               >Login</router-link
