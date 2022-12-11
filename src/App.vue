a2d349290f999998f211467d7148aabb05938fc5
wanda <vdking.168@gmail.com>
2022-12-11 15:28:30 +0700

    saving the token as a cookie

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@ import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import { store } from "./components/store.js";
 
+const getCookie = (name) => {
+  return document.cookie.split("; ").reduce((r, v) => {
+    const parts = v.split("=");
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+  }, "");
+};
+
 export default {
   name: "App",
   components: {
@@ -21,6 +28,23 @@ export default {
     return {
       store,
     };
+  },
+  beforeMount() {
+    // check for a cookie
+    let data = getCookie("_site_data");
+
+    if (data !== "") {
+      let cookieData = JSON.parse(data);
+
+      // update store
+      store.token = cookieData.token.token;
+      store.user = {
+        id: cookieData.user.id,
+        first_name: cookieData.user.first_name,
+        last_name: cookieData.user.last_name,
+        email: cookieData.user.email,
+      };
+    }
   },
 };
 </script>
