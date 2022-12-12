8f356724affa8b530d510ce3cff24b6ad6adcfdd
wanda <vdking.168@gmail.com>
2022-12-12 14:28:38 +0700

    displaying the list of all users

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,44 +1,67 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="mt-3">All Users</h1>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-3">All Users</h1>
+      </div>
+      <hr />
+
+      <table class="table table-compact table-striped">
+        <thead>
+          <tr>
+            <td>User</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="u in this.users" v-bind:key="u.id">
+            <td>
+              <router-link :to="`/admin/user/${u.id}`"
+                >{{ u.last_name }}, {{ u.first_name }}</router-link
+              >
+            </td>
+            <td>{{ u.email }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
-import Security from './security.js'
-import notie from 'notie'
+import Security from "./security.js";
+import notie from "notie";
 
 export default {
-    data() {
-        return {
-            users: [],
-        }
-    },
-    beforeMount() {
-        Security.requireToken();
+  data() {
+    return {
+      users: [],
+    };
+  },
+  beforeMount() {
+    Security.requireToken();
 
-        fetch(process.env.VUE_APP_API_URL + "/admin/users", Security.requestOptions(""))
-        .then((response) => response.json())
-        .then((response) => {
-            if (response.error) {
-                notie.alert({
-                    type: "error",
-                    text: response.message,
-                })
-            } else {
-                this.users = response.data.users;
-            }
-        })
-        .catch((error) => {
-            notie.alert({
-                type: "error",
-                text: error,
-            })
+    fetch(
+      process.env.VUE_APP_API_URL + "/admin/users",
+      Security.requestOptions("")
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.error) {
+          notie.alert({
+            type: "error",
+            text: response.message,
+          });
+        } else {
+          this.users = response.data.users;
+        }
+      })
+      .catch((error) => {
+        notie.alert({
+          type: "error",
+          text: error,
         });
-    }
-}
+      });
+  },
+};
 </script>
