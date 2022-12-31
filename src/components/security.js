bf63d980ec837831b944f35b950ca535cfcc8853
Wanda Kun <vdking.168@gmail.com>
2022-12-31 20:40:18 +0700

    trying out validate token handler and security checktoken functionality

--- src/components/security.js
+++ src/components/security.js
@@ -1,27 +1,62 @@
-import {store} from './store.js'
-import router from './../router/index.js'
+import { store } from "./store.js";
+import router from "./../router/index.js";
 
 let Security = {
-    // make sure user is authenticated
-    requireToken: function() {
-        if (store.token === '') {
-            router.push("/login");
-            return false
-        }
-    },
-
-    // create request options and send them back
-    requestOptions: function(payload) {
-        const headers = new Headers();
-        headers.append("Content-Type", "application/json");
-        headers.append("Authorization", "Bearer " + store.token);
-
-        return {
-            method: "POST",
-            body: JSON.stringify(payload),
-            headers: headers,
-        }
+  // make sure user is authenticated
+  requireToken: function () {
+    if (store.token === "") {
+      router.push("/login");
+      return false;
     }
-}
+  },
+
+  // create request options and send them back
+  requestOptions: function (payload) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Authorization", "Bearer " + store.token);
+
+    return {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: headers,
+    };
+  },
+
+  // check token
+  checkToken: function () {
+    if (store.token !== "") {
+      const payload = {
+        token: store.token,
+      };
+
+      const headers = new Headers();
+      headers.append("Content-Type", "application/json");
+
+      let requestOptions = {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: headers,
+      };
+
+      fetch(process.env.VUE_APP_API_URL + "/validate-token", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.error) {
+            console.log(data.error);
+          } else {
+            if (!data.data) {
+              store.token = "";
+              store.user = {};
+              document.cookie =
+                "_site_data=; Path=/;" +
+                "SameSite=strict; Secure;" +
+                "Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+            }
+          }
+        });
+    }
+  },
+};
 
 export default Security;
--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Body from './../components/Body.vue'
-import Login from './../components/Login.vue'
-import Books from './../components/Books.vue'
-import Book from './../components/Book.vue'
-import BooksAdmin from './../components/BooksAdmin.vue'
-import BookEdit from './../components/BookEdit.vue'
-import Users from './../components/Users.vue'
-import User from './../components/UserEdit.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Body from "./../components/Body.vue";
+import Login from "./../components/Login.vue";
+import Books from "./../components/Books.vue";
+import Book from "./../components/Book.vue";
+import BooksAdmin from "./../components/BooksAdmin.vue";
+import BookEdit from "./../components/BookEdit.vue";
+import Users from "./../components/Users.vue";
+import User from "./../components/UserEdit.vue";
+import Security from "./../components/security";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Body,
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-    },
-    {
-        path: '/books',
-        name: 'Books',
-        component: Books,
-    },
-    {
-        path: '/books/:bookName',
-        name: 'Book',
-        component: Book,
-    },
-    {
-        path: '/admin/books',
-        name: 'BooksAdmin',
-        component: BooksAdmin,
-    },
-    {
-        path: '/admin/books/:bookId',
-        name: 'BookEdit',
-        component: BookEdit,
-    },
-    {
-        path: '/admin/users',
-        name: 'Users',
-        component: Users,
-    },
-    {
-        path: '/admin/users/:userId',
-        name: 'User',
-        component: User,
-    },
-]
+  {
+    path: "/",
+    name: "Home",
+    component: Body,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/books",
+    name: "Books",
+    component: Books,
+  },
+  {
+    path: "/books/:bookName",
+    name: "Book",
+    component: Book,
+  },
+  {
+    path: "/admin/books",
+    name: "BooksAdmin",
+    component: BooksAdmin,
+  },
+  {
+    path: "/admin/books/:bookId",
+    name: "BookEdit",
+    component: BookEdit,
+  },
+  {
+    path: "/admin/users",
+    name: "Users",
+    component: Users,
+  },
+  {
+    path: "/admin/users/:userId",
+    name: "User",
+    component: User,
+  },
+];
 
-const router = createRouter({history: createWebHistory(), routes})
-export default router
+const router = createRouter({ history: createWebHistory(), routes });
+router.beforeEach(() => {
+  Security.checkToken();
+});
+export default router;
