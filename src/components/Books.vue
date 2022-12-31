e4264a467c75530751504ed9ab46c6a60cddbdd3
Wanda Kun <vdking.168@gmail.com>
2022-12-31 23:02:42 +0700

    displaying books on the front-end

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,9 +1,72 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="mt-3">Books</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-3">Books</h1>
+      </div>
+
+      <hr />
+
+      <div>
+        <div class="card-group">
+          <div class="p-3 d-flex flex-wrap">
+            <div v-for="b in this.books" :key="b.id">
+              <div
+                class="card me-3 ms-1 mb-3"
+                style="width: 10rem"
+                v-if="b?.gere_ids?.includes(currentFilter) || currentFilter === 0"
+              >
+                <img
+                  :src="`${this.imgPath}/covers/${b.slug}.jpg`"
+                  class="card-img-top"
+                  :alt="`cover for ${b.title}`"
+                />
+                <div class="card-body text-center">
+                  <h6 class="card-title">{{ b.title }}</h6>
+                  {{ b.author.author_name }}
+                </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
+
+<script>
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+      ready: false,
+      imgPath: process.env.VUE_APP_IMAGE_URL,
+      books: {},
+      currentFilter: 0,
+    };
+  },
+  emits: ["error"],
+  beforeMount() {
+    fetch(process.env.VUE_APP_API_URL + "/books")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          this.$emit("error", data.message);
+        } else {
+          this.books = data.data.books;
+          this.ready = true;
+        }
+      })
+      .catch((error) => {
+        this.$emit("error", error);
+      });
+  },
+  methods: {
+    filterBooks(filter) {
+      this.currentFilter = filter;
+    },
+  },
+};
+</script>
