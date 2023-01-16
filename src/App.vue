55e78409b42f2ba118d084871ead376e288a47b6
Wanda Kun <vdking.168@gmail.com>
2023-01-16 20:52:57 +0700

    saving the state of the books component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <Header />
   <div>
-    <router-view :key="componentKey" @success="success" @error="error" @warning="warning" @forceUpdate="forceUpdate" />
+    <router-view v-slot="{ Component}" :key="componentKey" @success="success" @error="error" @warning="warning" @forceUpdate="forceUpdate">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
   </div>
   <Footer />
 </template>
--- src/components/Book.vue
+++ src/components/Book.vue
@@ -3,21 +3,25 @@
     <div class="row">
       <div class="col-md-2">
         <img
+          v-if="this.ready"
           class="img-fluid img-thumbnail"
           :src="`${imgPath}/covers/${book.slug}.jpg`"
           alt="cover"
         />
       </div>
       <div class="col-md-10">
-        <h3 class="mt-3">{{ book.title }}</h3>
-        <hr />
-        <p>
-          <strong>Author:</strong> {{ book.author.author_name }} <br />
-          <strong>Published:</strong> {{ book.publication_year }} <br />
-        </p>
-        <p>
-          {{ book.description }}
-        </p>
+        <template v-if="this.ready">
+          <h3 class="mt-3">{{ book.title }}</h3>
+          <hr />
+          <p>
+            <strong>Author:</strong> {{ book.author.author_name }} <br />
+            <strong>Published:</strong> {{ book.publication_year }} <br />
+          </p>
+          <p>
+            {{ book.description }}
+          </p>
+        </template>
+        <p v-else>Loading...</p>
       </div>
     </div>
   </div>
@@ -30,9 +34,10 @@ export default {
     return {
       book: {},
       imgPath: process.env.VUE_APP_IMAGE_URL,
+      ready: false,
     };
   },
-  created() {
+  activated() {
     fetch(process.env.VUE_APP_API_URL + "/books/" + this.$route.params.bookName)
       .then((response) => response.json())
       .then((data) => {
@@ -40,11 +45,15 @@ export default {
           this.$emit("error", data.message);
         } else {
           this.book = data.data;
+          this.ready = true;
         }
       })
       .catch((error) => {
         this.$emit("error", error);
       });
   },
+  deactivated() {
+
+  }
 };
 </script>
