677295265df30dd5afa734fd6bc3bc304898d8e8
Wanda Kun <vdking.168@gmail.com>
2023-01-18 22:27:44 +0700

    listing all books for adminmstrator

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -53,7 +53,7 @@ export default {
       });
   },
   deactivated() {
-
-  }
+    this.ready = false;
+  },
 };
 </script>
--- src/components/BooksAdmin.vue
+++ src/components/BooksAdmin.vue
@@ -1,19 +1,62 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="mt-3">Books admin</h1>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-3">Manage Books</h1>
+        <hr />
+
+        <table v-if="this.ready" class="table table-striped table-compact">
+          <thead>
+            <tr>
+              <th>Book</th>
+              <th>Author</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="b in this.books" v-bind:key="b.id">
+              <td>
+                <router-link :to="`/admin/books/${b.id}`">{{
+                  b.title
+                }}</router-link>
+              </td>
+            <td>{{ b.author?.author_name }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import Security from './security.js'
 
 export default {
-    beforeMount() {
-        Security.requireToken();
-    }
-}
+  data() {
+    return {
+      books: {},
+      ready: false,
+    };
+  },
+  activated() {
+    Security.requireToken();
+
+    fetch(process.env.VUE_APP_API_URL + "/books")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          this.$emit("error", data.message);
+        } else {
+          this.books = data.data.books;
+          this.ready = true;
+        }
+      })
+      .catch((error) => {
+        this.$emit("error", error);
+      });
+  },
+  deactivated() {
+    this.ready = false;
+  },
+};
 </script>
