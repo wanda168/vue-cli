4507c484f9c664ea1113388f4aad1dc724ada4d8
Wanda Kun <vdking.168@gmail.com>
2023-01-27 21:29:27 +0700

    adding a book

--- src/components/BookEdit.vue
+++ src/components/BookEdit.vue
@@ -1,19 +1,235 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="mt-3">Book Edit</h1>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-3">Add/Edit Book</h1>
+        <hr />
+
+        <form-tag
+          @bookEditEvent="submitHandler"
+          name="bookForm"
+          event="bookEditEvent"
+        >
+          <div v-if="this.book.slug != ''" class="mb-3">
+            <img
+              :src="`${this.imgPath}/covers/${this.book.slug}.jpg`"
+              class="img-fluid img-thumbnail book-cover"
+              alt="cover"
+            />
+          </div>
+
+          <div class="mb-3">
+            <label for="formFile" class="form-label">Cover Image</label>
+            <input
+              v-if="this.book.id === 0"
+              ref="coverInput"
+              class="form-control"
+              type="file"
+              id="formFile"
+              required
+              accept="image/jpeg"
+              @change="loadCoverImage"
+            />
+            <input
+              v-else
+              ref="coverInput"
+              class="form-control"
+              type="file"
+              id="formFile"
+              accept="image/jpeg"
+              @change="loadCoverImage"
+            />
+          </div>
+
+          <text-input
+            v-model="book.title"
+            type="text"
+            required="true"
+            label="Title"
+            :value="book.title"
+            name="title"
+          ></text-input>
+
+          <select-input
+            name="author-id"
+            v-model="this.book.author_id"
+            :items="this.authors"
+            required="required"
+            label="Author"
+          ></select-input>
+
+          <text-input
+            v-model="book.publication_year"
+            type="text"
+            required="true"
+            label="Publication Year"
+            :value="book.publication_year"
+            name="publication_year"
+          ></text-input>
+
+          <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea
+              v-model="book.description"
+              class="form-control"
+              id="description"
+              rows="3"
+              required
+            ></textarea>
+          </div>
+
+          <div class="mb-3">
+            <label for="genres" class="form-label">Genres</label>
+            <select
+              ref="genres"
+              v-model="this.book.genre_ids"
+              class="form-select"
+              id="genres"
+              size="7"
+              multiple
+              required
+            >
+              <option v-for="g in this.genres" :value="g.value" :key="g.value">
+                {{ g.text }}
+              </option>
+            </select>
+          </div>
+
+          <hr />
+
+          <div class="float-start">
+            <input type="submit" class="btn btn-primary me-2" value="Save" />
+            <router-link to="/admin/books" class="btn btn-secondary"
+              >Cancel</router-link
+            >
+          </div>
+
+          <div class="float-end">
+            <a
+              v-if="this.book.id != 0"
+              class="btn btn-danger"
+              href="javascript:void(0);"
+              @click="confirmDelete(this.book.id)"
+              >Delete</a
+            >
+          </div>
+        </form-tag>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import Security from './security.js'
+import Security from "./security.js";
+import FormTag from "./forms/FormTag.vue";
+import TextInput from "./forms/TextInput.vue";
+import SelectInput from "./forms/SelectInput.vue";
+import router from "@/router";
+import notie from "notie";
 
 export default {
-    beforeMount() {
-        Security.requireToken();
-    }
-}
+  name: "BookEdit",
+  beforeMount() {
+    Security.requireToken();
+  },
+  components: {
+    "form-tag": FormTag,
+    "text-input": TextInput,
+    "select-input": SelectInput,
+  },
+  data() {
+    return {
+      book: {
+        id: 0,
+        title: "",
+        author_id: 0,
+        publication_year: 0,
+        description: "",
+        cover: "",
+        slug: "",
+        genres: [],
+        genre_ids: [],
+      },
+      authors: [],
+      imgPath: process.env.VUE_APP_IMG_PATH,
+      genres: [
+        { value: 1, text: "Science Fiction" },
+        { value: 2, text: "Fantasy" },
+        { value: 3, text: "Romance" },
+        { value: 4, text: "Thriller" },
+        { value: 5, text: "Mystry" },
+        { value: 6, text: "Horror" },
+        { value: 7, text: "Classic" },
+      ],
+    };
+  },
+  methods: {
+    submitHandler() {
+      const payload = {
+        id: this.book.id,
+        title: this.book.title,
+        author_id: parseInt(this.book.author_id, 10),
+        publication_year: this.book.publication_year,
+        description: this.book.description,
+        cover: this.book.cover,
+        slug: this.book.slug,
+        genre_ids: this.book.genre_ids,
+      };
+
+      fetch(
+        `${process.env.VUE_APP_API_URL}/admin/books/save`,
+        Security.requestOptions(payload)
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.error) {
+            this.$emit("error", data.error);
+          } else {
+            this.$emit("success", "Changes saved");
+            router.push("/admin/books");
+          }
+        })
+        .catch((error) => {
+          this.$emit("error", error);
+        });
+    },
+    loadCoverImage() {
+      let file = this.$refs.coverInput.files[0];
+
+      let reader = new FileReader();
+      reader.onload = () => {
+        const base64String = reader.result
+          .replace("data:", "")
+          .replace(/^.+,/, "");
+        this.book.cover = base64String;
+      };
+      reader.readAsDataURL(file);
+    },
+    confirmDelete(id) {
+      notie.confirm({
+        text: "Are you sure you want to delete this book?",
+        submitText: "Delete",
+        submitCallback: () => {
+          let payload = {
+            id: id,
+          };
+
+          fetch(process.env.VUE_APP_API_URL + "/admin/books/delete", Security.requestOptions(payload))
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.error) {
+                this.$emit("error", data.message);
+              } else {
+                this.$emit("success", "Book deleted");
+                router.push("/admin/books");
+              }
+            })
+            .catch((error) => {
+              this.$emit("error", error);
+            });
+        },
+      });
+    },
+  },
+};
 </script>
